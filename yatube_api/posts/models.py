bd6c0b2f046fa7836352a6bbbad796594d00fded
IvanPrudey <prudey.ia@yandex.ru>
2024-11-13 22:40:53 +0300

    correct_models

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -20,6 +20,8 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL, related_name="posts", blank=True, null=True
+    )
 
     def __str__(self):
         return self.text
@@ -42,3 +44,11 @@ class Follow(models.Model):
     following = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='following', null=True
     )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'following'],
+                name='unique subscription'
+            ),
+        ]

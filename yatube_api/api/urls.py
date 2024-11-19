201dc40971ef326e5d06a2c16c25f4d591b7a9f4
IvanPrudey <prudey.ia@yandex.ru>
2024-11-19 23:53:50 +0300

    add_verbose_name

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -15,6 +15,6 @@ router_ver_1.register('follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
     path('v1/', include(router_ver_1.urls)),
-    path('v1/', include('djoser.urls')),
     path('v1/', include('djoser.urls.jwt')),
+    # path('v1/', include(router_ver_1.urls, 'djoser.urls.jwt')),
 ]

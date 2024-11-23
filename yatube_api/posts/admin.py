e90ef04543cbdd09074ef609daa1e914d2f731df
IvanPrudey <prudey.ia@yandex.ru>
2024-11-23 15:05:44 +0300

    final_admin_zone_with_migrations

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,26 +1,30 @@
 from django.contrib import admin
 
-from .models import Group, Comment, Follow, Post
+from posts.models import Group, Comment, Follow, Post
 
 
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ('title',)
+    list_display = ('title', 'slug', 'description',)
     empty_value_display = 'Не задано'
+    search_fields = ('title',)
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('text',)
+    list_display = ('text', 'pub_date', 'author', 'group',)
     empty_value_display = 'Не задано'
+    search_fields = ('text', 'author',)
 
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('text',)
+    list_display = ('text', 'post', 'author', 'created')
     empty_value_display = 'Не задано'
+    search_fields = ('text', 'author',)
 
 
 class FollowAdmin(admin.ModelAdmin):
-    list_display = ('following',)
+    list_display = ('user', 'following',)
     empty_value_display = 'Не задано'
+    search_fields = ('user', 'following',)
 
 
 admin.site.register(Group, GroupAdmin)
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -5,43 +5,63 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    title = models.CharField('Название', max_length=200)
+    slug = models.SlugField('Слаг', unique=True)
+    description = models.TextField('Описание')
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
 
 class Post(models.Model):
-    text = models.TextField()
+    text = models.TextField('Текст')
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
+        User, on_delete=models.CASCADE, related_name='posts',
+        verbose_name='Автор'
+    )
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
     group = models.ForeignKey(
         Group, on_delete=models.SET_NULL,
-        related_name="posts", blank=True, null=True
+        related_name="posts", blank=True, null=True,
+        verbose_name='Группа'
     )
 
     def __str__(self):
         return self.text
 
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
 
 class Comment(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
+        User, on_delete=models.CASCADE, related_name='comments',
+        verbose_name='Автор'
+    )
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
+        Post, on_delete=models.CASCADE, related_name='comments',
+        verbose_name='Пост'
+    )
+    text = models.TextField('Текст')
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
 
 
 class Follow(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='follower', null=True
+        User, on_delete=models.CASCADE, related_name='follower', null=True,
+        verbose_name='Пользователь'
     )
     following = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='following', null=True
@@ -54,3 +74,5 @@ class Follow(models.Model):
                 name='unique subscription'
             ),
         ]
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'

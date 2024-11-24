094fa9ebc166501d3618dfa029683ffeeae32506
IvanPrudey <prudey.ia@yandex.ru>
2024-11-24 22:17:56 +0300

    correct_admin_with_decorator_register

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -3,31 +3,29 @@ from django.contrib import admin
 from posts.models import Group, Comment, Follow, Post
 
 
+@admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'description',)
     empty_value_display = 'Не задано'
     search_fields = ('title',)
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('text', 'pub_date', 'author', 'group',)
     empty_value_display = 'Не задано'
     search_fields = ('text', 'author',)
 
 
+@admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('text', 'post', 'author', 'created')
     empty_value_display = 'Не задано'
     search_fields = ('text', 'author',)
 
 
+@admin.register(Follow)
 class FollowAdmin(admin.ModelAdmin):
     list_display = ('user', 'following',)
     empty_value_display = 'Не задано'
     search_fields = ('user', 'following',)
-
-
-admin.site.register(Group, GroupAdmin)
-admin.site.register(Post, PostAdmin)
-admin.site.register(Comment, CommentAdmin)
-admin.site.register(Follow, FollowAdmin)
